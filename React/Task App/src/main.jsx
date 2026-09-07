4411c330916917c6785bcf88114f679f9580c5bd
Malik Yasir <my4871767@gmail.com>
2026-09-07 18:11:45 +0500

    Making Apis Calling

--- React/Task App/src/main.jsx
+++ React/Task App/src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
@@ -8,13 +7,11 @@ import TaskProvider from "./Context/TaskContext";
 import App from "./App";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <TaskProvider>
-          <App />
-        </TaskProvider>
-      </Provider>
-    </BrowserRouter>
-  </StrictMode>
+  <BrowserRouter>
+    <Provider store={store}>
+      <TaskProvider>
+        <App />
+      </TaskProvider>
+    </Provider>
+  </BrowserRouter>
 );
